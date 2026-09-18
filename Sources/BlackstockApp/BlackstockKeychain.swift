077dfdc846c03b8ce3540540d152bd958100ab74
kevinaklilu-cpu <kevin.aklilu@gmail.com>
2026-09-18 23:49:51 +0200

    Add secure Blackstock Keychain storage

---
+++ Sources/BlackstockApp/BlackstockKeychain.swift
@@ -0,0 +1,40 @@
+#if os(macOS)
+import Foundation
+import Security
+
+enum BlackstockKeychain {
+    private static let service = "de.blackstock.app"
+
+    static func read(_ account: String) -> String {
+        let query: [String: Any] = [
+            kSecClass as String: kSecClassGenericPassword,
+            kSecAttrService as String: service,
+            kSecAttrAccount as String: account,
+            kSecReturnData as String: true,
+            kSecMatchLimit as String: kSecMatchLimitOne
+        ]
+        var result: CFTypeRef?
+        guard SecItemCopyMatching(query as CFDictionary, &result) == errSecSuccess,
+              let data = result as? Data,
+              let value = String(data: data, encoding: .utf8) else { return "" }
+        return value
+    }
+
+    static func write(_ value: String, account: String) throws {
+        let data = Data(value.utf8)
+        let lookup: [String: Any] = [
+            kSecClass as String: kSecClassGenericPassword,
+            kSecAttrService as String: service,
+            kSecAttrAccount as String: account
+        ]
+        SecItemDelete(lookup as CFDictionary)
+        var insert = lookup
+        insert[kSecValueData as String] = data
+        insert[kSecAttrAccessible as String] = kSecAttrAccessibleAfterFirstUnlockThisDeviceOnly
+        let status = SecItemAdd(insert as CFDictionary, nil)
+        guard status == errSecSuccess else { throw KeychainError.status(status) }
+    }
+
+    enum KeychainError: Error { case status(OSStatus) }
+}
+#endif
