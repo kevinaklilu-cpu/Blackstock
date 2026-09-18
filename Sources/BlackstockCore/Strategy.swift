37ae184b23e88534c17e53e63ca547173350575c
kevinaklilu-cpu <kevin.aklilu@gmail.com>
2026-09-18 23:53:31 +0200

    Add explicit canonical strategy initializer and revision semantics

--- Sources/BlackstockCore/Strategy.swift
+++ Sources/BlackstockCore/Strategy.swift
@@ -34,6 +34,46 @@ public struct ChannelStrategy: Codable, Sendable, Equatable {
     public var explorationPolicy: ExplorationPolicy
     public var effectiveFrom: Date
     public var version: Int
+
+    public init(
+        channelID: String,
+        primaryTopic: String,
+        topicDefinition: String,
+        contentPromise: String,
+        pillars: [String],
+        adjacentTopics: [String],
+        excludedTopics: [String],
+        defaultContentLanguage: String,
+        researchLanguages: [String],
+        audienceHypothesis: String,
+        objectives: [StrategicObjective],
+        explorationPolicy: ExplorationPolicy,
+        effectiveFrom: Date,
+        version: Int
+    ) {
+        self.channelID = channelID
+        self.primaryTopic = primaryTopic
+        self.topicDefinition = topicDefinition
+        self.contentPromise = contentPromise
+        self.pillars = pillars
+        self.adjacentTopics = adjacentTopics
+        self.excludedTopics = excludedTopics
+        self.defaultContentLanguage = defaultContentLanguage
+        self.researchLanguages = researchLanguages
+        self.audienceHypothesis = audienceHypothesis
+        self.objectives = objectives.isEmpty ? [.balanced] : objectives
+        self.explorationPolicy = explorationPolicy
+        self.effectiveFrom = effectiveFrom
+        self.version = max(version, 1)
+    }
+
+    public func revised(at date: Date, mutate: (inout ChannelStrategy) -> Void) -> ChannelStrategy {
+        var copy = self
+        mutate(&copy)
+        copy.version += 1
+        copy.effectiveFrom = date
+        return copy
+    }
 }
 
 public struct ContentLanguageStrategy: Codable, Sendable, Equatable {
