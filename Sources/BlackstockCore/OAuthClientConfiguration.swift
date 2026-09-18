9042d4fbe0636d3027476e12a56d9f2c3e582efc
kevinaklilu-cpu <kevin.aklilu@gmail.com>
2026-09-18 23:40:15 +0200

    Canonical rebuild: Sources/BlackstockCore/OAuthClientConfiguration.swift

---
+++ Sources/BlackstockCore/OAuthClientConfiguration.swift
@@ -0,0 +1,26 @@
+import Foundation
+
+public enum OAuthClientConfigurationError: Error, Equatable, Sendable { case invalidJSON, unsupportedClientType, missingClientID, invalidClientID }
+
+public struct OAuthClientConfiguration: Sendable, Equatable {
+    public let clientID: String
+    public let projectID: String?
+    public let redirectURIs: [String]
+    public static func parseGoogleDesktopJSON(_ data: Data) throws -> OAuthClientConfiguration {
+        let envelope: Envelope
+        do { envelope = try JSONDecoder().decode(Envelope.self, from: data) } catch { throw OAuthClientConfigurationError.invalidJSON }
+        guard let installed = envelope.installed else { throw OAuthClientConfigurationError.unsupportedClientType }
+        let id = installed.clientID.trimmingCharacters(in: .whitespacesAndNewlines)
+        guard !id.isEmpty else { throw OAuthClientConfigurationError.missingClientID }
+        guard id.hasSuffix(".apps.googleusercontent.com") else { throw OAuthClientConfigurationError.invalidClientID }
+        return OAuthClientConfiguration(clientID: id, projectID: installed.projectID, redirectURIs: installed.redirectURIs ?? [])
+    }
+}
+
+private struct Envelope: Decodable { let installed: Installed? }
+private struct Installed: Decodable {
+    let clientID: String
+    let projectID: String?
+    let redirectURIs: [String]?
+    enum CodingKeys: String, CodingKey { case clientID = "client_id"; case projectID = "project_id"; case redirectURIs = "redirect_uris" }
+}
