1b9bd95ae8e59f0bc57d846809a244b18cb895b3
kevinaklilu-cpu <kevin.aklilu@gmail.com>
2026-09-20 14:02:14 +0200

    Model persistent supplemental audio mix settings

--- Sources/BlackstockCore/SupplementalCaptureAsset.swift
+++ Sources/BlackstockCore/SupplementalCaptureAsset.swift
@@ -46,3 +46,49 @@ public struct SupplementalCaptureAsset:
             && !rightsEvidence.isEmpty
     }
 }
+
+
+public struct SupplementalAudioMixSetting:
+    Codable,
+    Sendable,
+    Equatable,
+    Identifiable {
+
+    public let captureID: UUID
+    public var enabled: Bool
+    public var volume: Double
+
+    public var id: UUID { captureID }
+
+    public init(
+        captureID: UUID,
+        enabled: Bool = false,
+        volume: Double = 0.75
+    ) {
+        self.captureID = captureID
+        self.enabled = enabled
+        self.volume = min(max(volume, 0), 1)
+    }
+}
+
+public struct SupplementalAudioMixInput:
+    Sendable,
+    Equatable,
+    Identifiable {
+
+    public let captureID: UUID
+    public let fileURL: URL
+    public let volume: Double
+
+    public var id: UUID { captureID }
+
+    public init(
+        captureID: UUID,
+        fileURL: URL,
+        volume: Double
+    ) {
+        self.captureID = captureID
+        self.fileURL = fileURL
+        self.volume = min(max(volume, 0), 1)
+    }
+}
